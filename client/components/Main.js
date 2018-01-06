7c2b62eee80102c69df8acd0316cf55fa6f22a53
Roy Jossfolk <rjossfolk@gmail.com>
2018-01-06 17:41:12 -0500

    - add Main, PhotGrid, Single components + wire up Main.js

---
+++ client/components/Main.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Link } from 'react-router';
+
+const Main = React.craeteClass({
+    render() {
+        return (
+            <div>
+                <Link to="/">Reduxstagram</Link>
+            </div>
+        )
+    }
+});
+
+export default Main;
